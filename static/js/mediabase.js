99b044d246397f50bef38232aea5404757791415
Chris Blow <unthinkingly@gmail.com>
2010-01-29 16:43:04 -0800

    new editor bar and in place editor script.

--- static/js/mediabase.js
+++ static/js/mediabase.js
@@ -1,25 +1,34 @@
-// =====================
-// = mediabase domains =
-// =====================
-jQuery("span.mediabase_domain a").each(function() {
-  jQuery(this).qtip( {
-     content: '<em>Mediabase Summary:</em> Owner=King of England, Headquarters=London, England <br /> <em>Source Languages</em> = en/ar <br /> <a href="#">Mediabse Entry</a><br /> <a href=""><em>Create</em> an Arabic Wikipedia Entry</a> <br /> <a href="#">Original Article</a> <br /> <a href="#">Translation into Arabic</a>',
-     position: 'topRight',
-     hide: { fixed: true},
-     style: { padding: '5px 15px'}
-  });
-});
+  $ = jQuery
 
-// ===================
-// = unknown domains =
-// ===================
-
-// Use the each() method to gain access to each of the elements attributes
-jQuery("span.unknown_domain a").each(function() {
-  jQuery(this).qtip( {
-     content: '<em>There is no Mediabase entry on this source.</p> <a href="http://freebase.com">Create a Mediabase Page in Arabic or Engligh</a>.',
-     position: 'topRight',
-     hide: { fixed: true},
-     style: { padding: '5px 15px'}
+  // =====================
+  // = mediabase domains =
+  // =====================
+  $("span.mediabase_domain a").each(function() {
+    $(this).qtip( {
+       content: '<em>Mediabase Summary:</em> Owner=King of England, Headquarters=London, England <br /> <em>Source Languages</em> = en/ar <br /> <a href="#">Mediabse Entry</a><br /> <a href=""><em>Create</em> an Arabic Wikipedia Entry</a> <br /> <a href="#">Original Article</a> <br /> <a href="#">Translation into Arabic</a>',
+       position: 'topRight',
+       hide: { fixed: true},
+       style: { padding: '5px 15px'}
+    });
   });
-});
+
+  // ===================
+  // = unknown domains =
+  // ===================
+
+  // Use the each() method to gain access to each of the elements attributes
+  $("span.unknown_domain a").each(function() {
+    $(this).qtip( {
+       content: '<em>There is no Mediabase entry on this source.</p> <a href="http://freebase.com">Create a Mediabase Page in Arabic or Engligh</a>.',
+       position: 'topRight',
+       hide: { fixed: true},
+       style: { padding: '5px 15px'}
+    });
+  });
+
+
+  // ==============
+  // = editor bar =
+  // ==============
+  
+  $('#container').prepend('<form id="editor_bar"><span>Name:<input></input></span>  <span>Language:<input></input></span>  <span>Owner:<input></input></span> </form>');
